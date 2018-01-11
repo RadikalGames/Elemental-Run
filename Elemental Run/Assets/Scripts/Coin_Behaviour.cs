32ac2bab26db69d2eafd5a8e858e07b3c16cddc2
Souryadeep Mukherjee <secondary.sd@gmail.com>
2018-01-11 23:21:48 +0530

    5th commit
    Added coin generation and fixed issue 1.

---
+++ Elemental Run/Assets/Scripts/Coin_Behaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Behaviour : MonoBehaviour 
+{
+	private ScoreManager instance;
+	private GameObject g;
+
+	// Use this for initialization
+	void Start () 
+	{
+		g = GameObject.Find ("ScoreManager");
+		instance = g.GetComponent<ScoreManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Player") 
+		{
+			instance.Score += 1;
+			gameObject.SetActive (false);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+	}
+}
